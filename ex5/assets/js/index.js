115cb64fa59279c6fb2184cd5b4f3cdebdfde4ca
PhanTrongNhat <48424808+PhanTrongNhat@users.noreply.github.com>
2022-03-24 01:35:03 +0700

    add

--- ex5/assets/js/index.js
+++ ex5/assets/js/index.js
@@ -6,78 +6,150 @@ const border_carousel = document.getElementsByClassName("border_carousel");
 const carouselBottom = document.getElementsByClassName("carousel_bottom");
 const bordercarousel = document.getElementsByClassName("border_carousel");
 console.log(bordercarousel[0].style.width);
-carousel[0].style.width = `${length * 220}px`;
-carouselBottom[0].style.width = `${(lengthBottom * 220)}px`;
- carouselBottom[0].style.marginLeft = ""; 
-function changeSlide() {
- 
- 
-  if (carousel[0].style.marginLeft.length !== 0) {
-     let subValue = carousel[0].style.marginLeft.slice(
-       0,
-       carousel[0].style.marginLeft.length - 2
-     );
-      
-    subValue =parseInt( subValue.slice(1, subValue.length));
+carousel[0].style.width = `${length * 245}px`;
+carouselBottom[0].style.width = `${lengthBottom * 245}px`;
+carouselBottom[0].style.marginLeft = "";
+function changeSlide(value) {
+  if (value === 1) {
+    if (carousel[0].style.marginLeft.length !== 0) {
+      let subValue = carousel[0].style.marginLeft.slice(
+        0,
+        carousel[0].style.marginLeft.length - 2
+      );
 
-    const widthCarousel = parseInt(
-      carousel[0].style.width.slice(0, carousel[0].style.width.length - 2)
-    );
-    const widthBorder = border_carousel[0].clientWidth;
+      subValue = parseInt(subValue.slice(1, subValue.length));
 
+      const widthCarousel = parseInt(
+        carousel[0].style.width.slice(0, carousel[0].style.width.length - 2)
+      );
+      const widthBorder = border_carousel[0].clientWidth;
 
-    console.log( widthCarousel, widthBorder,  subValue);
+      // console.log(widthCarousel, widthBorder, subValue);
 
-    const tempCompare = widthCarousel - widthBorder - subValue;
+      const tempCompare = widthCarousel - widthBorder - subValue;
 
-    console.log(typeof tempCompare);
-    if (tempCompare > 0) {
-      carousel[0].style.marginLeft = `-${subValue + 220}px`;
-    }else{
-       carousel[0].style.marginLeft = "";
+      // console.log(typeof tempCompare);
+      if (tempCompare > 0) {
+        carousel[0].style.marginLeft = `-${subValue + 245}px`;
+      }
+      //   else {
+      //    carousel[0].style.marginLeft = "";
+      //  }
+    } else {
+      const widthCarousel = parseInt(
+        carousel[0].style.width.slice(0, carousel[0].style.width.length - 2)
+      );
+      const widthBorder = border_carousel[0].clientWidth;
+      const tempCompare = widthCarousel - widthBorder;
+
+      console.log(typeof tempCompare);
+      if (tempCompare > 0) {
+        carousel[0].style.marginLeft = "-245px";
+      } else {
+        carousel[0].style.marginLeft = "";
+      }
     }
-    
   } else {
-    carousel[0].style.marginLeft = "-220px";
+    if (carousel[0].style.marginLeft.length !== 0) {
+      let subValue = carousel[0].style.marginLeft.slice(
+        0,
+        carousel[0].style.marginLeft.length - 2
+      );
+
+      subValue = parseInt(subValue.slice(1, subValue.length));
+      carousel[0].style.marginLeft = `-${subValue - 245}px`;
+      if (carousel[0].style.marginLeft === "0px") {
+        carousel[0].style.marginLeft = "";
+      }
+    } else {
+      carousel[0].style.marginLeft = "";
+    }
+  }
+}
+function changeSlideBottom(value) {
+  if (value === 1) {
+    if (carouselBottom[0].style.marginLeft.length !== 0) {
+      let subValue = carouselBottom[0].style.marginLeft.slice(
+        0,
+        carouselBottom[0].style.marginLeft.length - 2
+      );
+
+      subValue = parseInt(subValue.slice(1, subValue.length));
+
+      const widthcarouselBottom = parseInt(
+        carouselBottom[0].style.width.slice(0, carouselBottom[0].style.width.length - 2)
+      );
+      const widthBorder = border_carousel[1].clientWidth;
+
+      // console.log(widthcarouselBottom, widthBorder, subValue);
+
+      const tempCompare = widthcarouselBottom - widthBorder - subValue;
+
+      // console.log(typeof tempCompare);
+      if (tempCompare > 0) {
+        carouselBottom[0].style.marginLeft = `-${subValue + 245}px`;
+      }
+      //   else {
+      //    carouselBottom[0].style.marginLeft = "";
+      //  }
+    } else {
+      const widthcarouselBottom = parseInt(
+        carouselBottom[0].style.width.slice(0, carouselBottom[0].style.width.length - 2)
+      );
+      const widthBorder = border_carousel[1].clientWidth;
+      const tempCompare = widthcarouselBottom - widthBorder;
+
+      console.log(typeof tempCompare);
+      if (tempCompare > 0) {
+        carouselBottom[0].style.marginLeft = "-245px";
+      } else {
+        carouselBottom[0].style.marginLeft = "";
+      }
+    }
+  } else {
+    if (carouselBottom[0].style.marginLeft.length !== 0) {
+      let subValue = carouselBottom[0].style.marginLeft.slice(
+        0,
+        carouselBottom[0].style.marginLeft.length - 2
+      );
+
+      subValue = parseInt(subValue.slice(1, subValue.length));
+      carouselBottom[0].style.marginLeft = `-${subValue - 245}px`;
+      if (carouselBottom[0].style.marginLeft === "0px") {
+        carouselBottom[0].style.marginLeft = "";
+      }
+    } else {
+      carousel[0].style.marginLeft = "";
+    }
   }
 
-  
-}
-function changeSlideBottom() {
-  // const subValue = carouselBottom[0].style.marginLeft.slice(
-  //   0,
-  //   carouselBottom[0].style.marginLeft.length - 2
-  // );
-  // console.log(subValue);
-  // carouselBottom[0].style.marginLeft = `${subValue - 220}px`;
+  // if (carouselBottom[0].style.marginLeft.length !== 0) {
+  //   let subValue = carouselBottom[0].style.marginLeft.slice(
+  //     0,
+  //     carouselBottom[0].style.marginLeft.length - 2
+  //   );
 
+  //   subValue = parseInt(subValue.slice(1, subValue.length));
+  //   console.log(carouselBottom[0].style);
+  //   const widthCarousel = parseInt(
+  //     carouselBottom[0].style.width.slice(
+  //       0,
+  //       carouselBottom[0].style.width.length - 2
+  //     )
+  //   );
+  //   const widthBorder = border_carousel[0].clientWidth;
 
-   if (carouselBottom[0].style.marginLeft.length !== 0) {
- 
-     let subValue = carouselBottom[0].style.marginLeft.slice(
-       0,
-       carouselBottom[0].style.marginLeft.length - 2
-     );
-  
-     subValue = parseInt(subValue.slice(1, subValue.length));
- console.log(carouselBottom[0].style);
-     const widthCarousel = parseInt(
-       carouselBottom[0].style.width.slice(0, carouselBottom[0].style.width.length - 2)
-     );
-     const widthBorder = border_carousel[0].clientWidth;
+  //   console.log(widthCarousel, widthBorder, subValue);
 
-     console.log(widthCarousel, widthBorder, subValue);
+  //   const tempCompare = widthCarousel - widthBorder - subValue;
 
-     const tempCompare = widthCarousel - widthBorder - subValue;
-
-     console.log(typeof tempCompare);
-     if (tempCompare > 0) {
-       carouselBottom[0].style.marginLeft = `-${subValue + 220}px`;
-     } else {
-       carouselBottom[0].style.marginLeft = "";
-     }
-   } else {
-
-     carouselBottom[0].style.marginLeft = "-220px";
-   }
+  //   console.log(typeof tempCompare);
+  //   if (tempCompare > 0) {
+  //     carouselBottom[0].style.marginLeft = `-${subValue + 220}px`;
+  //   } else {
+  //     carouselBottom[0].style.marginLeft = "";
+  //   }
+  // } else {
+  //   carouselBottom[0].style.marginLeft = "-220px";
+  // }
 }
